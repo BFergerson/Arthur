11cf821781257f1b26f4ffe9767a358da8e5928e
Brandon Fergerson <brandon.fergerson@codebrig.com>
2018-11-07 08:54:48 -0800

    relative

--- src/test/groovy/com/codebrig/omnisrc/generator/SchemaGeneratorTest.groovy
+++ src/test/groovy/com/codebrig/omnisrc/generator/SchemaGeneratorTest.groovy
@@ -8,6 +8,8 @@ import com.codebrig.omnisrc.structure.filter.RoleFilter
 import com.codebrig.omnisrc.structure.filter.TypeFilter
 import org.junit.Test
 
+import static org.junit.Assert.assertEquals
+
 class SchemaGeneratorTest {
 
     @Test
@@ -19,20 +21,14 @@ class SchemaGeneratorTest {
         multiFilter.acceptFilter(new TypeFilter("MethodDeclaration"))
         schemaGenerator.filter = multiFilter
 
-        def goLanguage = schemaGenerator.observeLanguage(SourceLanguage.Go,
-                new File("/home/brandon/IdeaProjects/OmniSRC/src/test/resources/same"))
-        def javaLanguage = schemaGenerator.observeLanguage(SourceLanguage.Java,
-                new File("/home/brandon/IdeaProjects/OmniSRC/src/test/resources/same"))
-        def javascriptLanguage = schemaGenerator.observeLanguage(SourceLanguage.Javascript,
-                new File("/home/brandon/IdeaProjects/OmniSRC/src/test/resources/same"))
-        def pythonLanguage = schemaGenerator.observeLanguage(SourceLanguage.Python,
-                new File("/home/brandon/IdeaProjects/OmniSRC/src/test/resources/same"))
+        def goLanguage = schemaGenerator.observeLanguage(SourceLanguage.Go, new File("src/test/resources/same/"))
+        def javaLanguage = schemaGenerator.observeLanguage(SourceLanguage.Java, new File("src/test/resources/same/"))
+        def javascriptLanguage = schemaGenerator.observeLanguage(SourceLanguage.Javascript, new File("src/test/resources/same/"))
+        def pythonLanguage = schemaGenerator.observeLanguage(SourceLanguage.Python, new File("src/test/resources/same/"))
         def omniLanguage = OmniObservedLanguage.makeOmniObservedLanguage(goLanguage, javaLanguage, javascriptLanguage, pythonLanguage)
         def schemaWriter = new GraknSchemaWriter(omniLanguage, goLanguage, javaLanguage, javascriptLanguage, pythonLanguage)
-        def outputFile = new File("/home/brandon/IdeaProjects/OmniSRC/src/test/resources/same/test.gql")
-        if (outputFile.exists()) outputFile.delete()
-        outputFile.createNewFile()
-        outputFile.write(schemaWriter.getSchemaDefinition())
+        def verifyFile = new File("src/test/resources/same/", "Same_Schema.gql")
+        assertEquals(verifyFile.text, schemaWriter.getSchemaDefinition())
     }
 
 }
