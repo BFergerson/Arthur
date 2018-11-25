f62ccd3ab08a6e0bdf89642f1f5ef11f5bd6e5b1
Brandon Fergerson <brandon.fergerson@codebrig.com>
2018-11-24 21:40:27 -0800

    best as i understand it

--- src/main/groovy/com/codebrig/omnisrc/observe/structure/literal/JavaLiteral.groovy
+++ src/main/groovy/com/codebrig/omnisrc/observe/structure/literal/JavaLiteral.groovy
@@ -2,6 +2,7 @@ package com.codebrig.omnisrc.observe.structure.literal
 
 import com.codebrig.omnisrc.SourceNode
 import com.codebrig.omnisrc.observe.structure.StructureLiteral
+import org.apache.commons.lang.math.NumberUtils
 
 /**
  * Used to determine and get the literal type of Java AST nodes
@@ -16,8 +17,11 @@ class JavaLiteral extends StructureLiteral {
     String getNodeLiteralAttribute(SourceNode node) {
         switch (Objects.requireNonNull(node).internalType) {
             case "NumberLiteral":
-                if (!node.token.toUpperCase().contains("X") && (node.token.contains(".") ||
-                        node.token.toUpperCase().endsWith("D") || node.token.toUpperCase().endsWith("F"))) {
+                if (node.token.contains(".")
+                        || node.token.toUpperCase().contains("P")
+                        || node.token.toUpperCase().contains("E")
+                        || node.token.toUpperCase().endsWith("D")
+                        || node.token.toUpperCase().endsWith("F")) {
                     return floatValueLiteral()
                 }
                 return numberValueLiteral()
@@ -33,6 +37,30 @@ class JavaLiteral extends StructureLiteral {
                 return [numberValueLiteral(), floatValueLiteral()]
             default:
                 return Collections.emptyList()
+        }
+    }
+
+    static long toLong(String value) {
+        value = value.replace("_", "")
+        try {
+            if (value.toUpperCase().startsWith("0X") && value.toUpperCase().endsWith("L")) {
+                return new BigInteger(value.substring(2, value.length() - 1), 16).longValue()
+            }
+            if (value.toUpperCase().endsWith("L")) {
+                return Long.decode(value.substring(0, value.length() - 1))
+            }
+            return Long.valueOf(value)
+        } catch (Exception ex) {
+            return NumberUtils.toLong(value)
+        }
+    }
+
+    static double toDouble(String value) {
+        value = value.replace("_", "")
+        try {
+            return Double.valueOf(value)
+        } catch (Exception ex) {
+            return NumberUtils.toDouble(value)
         }
     }
 }
