8d9de6e428008fdeca767e01347d1076d89e6434
Brandon Fergerson <brandon.fergerson@codebrig.com>
2018-11-24 21:44:20 -0800

    default impl

--- src/main/groovy/com/codebrig/omnisrc/observe/structure/StructureLiteral.groovy
+++ src/main/groovy/com/codebrig/omnisrc/observe/structure/StructureLiteral.groovy
@@ -1,6 +1,7 @@
 package com.codebrig.omnisrc.observe.structure
 
 import com.codebrig.omnisrc.SourceNode
+import org.apache.commons.lang.math.NumberUtils
 
 /**
  * Used to determine and get the literal type of UAST nodes
@@ -10,6 +11,30 @@ import com.codebrig.omnisrc.SourceNode
  * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
  */
 abstract class StructureLiteral {
+
+    long toLong(String value) {
+        value = value.replace("_", "")
+        try {
+            if (value.toUpperCase().startsWith("0X") && value.toUpperCase().endsWith("L")) {
+                return new BigInteger(value.substring(2, value.length() - 1), 16).longValue()
+            }
+            if (value.toUpperCase().endsWith("L")) {
+                return Long.decode(value.substring(0, value.length() - 1))
+            }
+            return Long.valueOf(value)
+        } catch (Exception ex) {
+            return NumberUtils.toLong(value)
+        }
+    }
+
+    double toDouble(String value) {
+        value = value.replace("_", "")
+        try {
+            return Double.valueOf(value)
+        } catch (Exception ex) {
+            return NumberUtils.toDouble(value)
+        }
+    }
 
     boolean isNodeLiteral(SourceNode node) {
         return getNodeLiteralAttribute(node) != null
--- src/main/groovy/com/codebrig/omnisrc/observe/structure/literal/JavaLiteral.groovy
+++ src/main/groovy/com/codebrig/omnisrc/observe/structure/literal/JavaLiteral.groovy
@@ -2,7 +2,6 @@ package com.codebrig.omnisrc.observe.structure.literal
 
 import com.codebrig.omnisrc.SourceNode
 import com.codebrig.omnisrc.observe.structure.StructureLiteral
-import org.apache.commons.lang.math.NumberUtils
 
 /**
  * Used to determine and get the literal type of Java AST nodes
@@ -37,30 +36,6 @@ class JavaLiteral extends StructureLiteral {
                 return [numberValueLiteral(), floatValueLiteral()]
             default:
                 return Collections.emptyList()
-        }
-    }
-
-    static long toLong(String value) {
-        value = value.replace("_", "")
-        try {
-            if (value.toUpperCase().startsWith("0X") && value.toUpperCase().endsWith("L")) {
-                return new BigInteger(value.substring(2, value.length() - 1), 16).longValue()
-            }
-            if (value.toUpperCase().endsWith("L")) {
-                return Long.decode(value.substring(0, value.length() - 1))
-            }
-            return Long.valueOf(value)
-        } catch (Exception ex) {
-            return NumberUtils.toLong(value)
-        }
-    }
-
-    static double toDouble(String value) {
-        value = value.replace("_", "")
-        try {
-            return Double.valueOf(value)
-        } catch (Exception ex) {
-            return NumberUtils.toDouble(value)
         }
     }
 }
