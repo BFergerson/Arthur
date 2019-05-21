cfa0cdf64239aa4e8e20bc084cf06e9f1c1a5b76
chess-equality <chessequality@gmail.com>
2019-05-21 08:46:25 +0800

    - [#30] Implemented for Java

--- src/test/resources/same/functions/Functions.java
+++ src/test/resources/same/functions/Functions.java
@@ -7,4 +7,36 @@ public class Functions {
     public void function2() {
         System.out.println("");
     }
+
+    public void function3(int var) {
+        System.out.println("function3(int var)");
+    }
+
+    public void function4(int... var) {
+        System.out.println("function4(int... var)");
+    }
+
+    public void function5(int var[]) {
+        System.out.println("function5(int var[])");
+    }
+
+    public void function6(int[] var) {
+        System.out.println("function6(int[] var)");
+    }
+
+    public void function7(Integer[] var) {
+        System.out.println("function7(Integer[] var)");
+    }
+
+    public <T> void function8(T t) {
+        System.out.println("function8(T t)");
+    }
+
+    public <T> void function9(List<T> var) {
+        System.out.println("function9(List<T> var)");
+    }
+
+    public void function10(List<Integer> var) {
+        System.out.println("function10(List<Integer> var)");
+    }
 }
