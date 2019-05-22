ef53a4ef07cd7cb4b3fbbe0999875fea5e6fa352
chess-equality <chessequality@gmail.com>
2019-05-23 06:08:22 +0800

    - [#30] Implemented for Go

--- src/test/resources/same/functions/Functions.go
+++ src/test/resources/same/functions/Functions.go
@@ -2,10 +2,52 @@ package main
 
 import "fmt"
 
+type receiverType string
+
 func function1() {
     fmt.Println("")
 }
 
 func function2() {
     fmt.Println("")
+}
+
+func function3(v uint64) {
+    fmt.Println("function3(v uint64)")
+}
+
+func function4(v ...uint64) {
+    fmt.Println("function4(v ...uint64)")
+}
+
+func function5(v [3]uint64) {
+    fmt.Println("function5(v [3]uint64)")
+}
+
+func function6(v []uint64) {
+    fmt.Println("function6(v []uint64)")
+}
+
+func Function7(v uint64) {
+    fmt.Println("Function7(v uint64)")
+}
+
+func (receiverType) function8(v uint64) {
+    fmt.Println("function8(v uint64)")
+}
+
+func (t receiverType) function9(v uint64) {
+    fmt.Println("function9(v uint64)")
+}
+
+func function10(v *uint64) {
+    fmt.Println("function10(v *uint64)")
+}
+
+func (*receiverType) function11(v uint64) {
+    fmt.Println("function11(v uint64)")
+}
+
+func (t *receiverType) function12(v uint64) {
+    fmt.Println("function12(v uint64)")
 }
