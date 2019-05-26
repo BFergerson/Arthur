026c9d8c083aaa1190d59e02eab55573b633eaa1
chess-equality <chessequality@gmail.com>
2019-05-26 23:22:50 +0800

    - [#30] Minor refactoring

--- src/test/resources/same/functions/Functions.java
+++ src/test/resources/same/functions/Functions.java
@@ -9,34 +9,34 @@ public class Functions {
     }
 
     public void function3(int var) {
-        System.out.println("function3(int var)");
+        System.out.println("intArg");
     }
 
     public void function4(int... var) {
-        System.out.println("function4(int... var)");
+        System.out.println("intVarargs");
     }
 
     public void function5(int var[]) {
-        System.out.println("function5(int var[])");
+        System.out.println("intArray1Arg");
     }
 
     public void function6(int[] var) {
-        System.out.println("function6(int[] var)");
+        System.out.println("intArray2Arg");
     }
 
     public void function7(Integer[] var) {
-        System.out.println("function7(Integer[] var)");
+        System.out.println("integerArrayArg");
     }
 
     public <T> void function8(T t) {
-        System.out.println("function8(T t)");
+        System.out.println("genericsType1Arg");
     }
 
     public <T> void function9(List<T> var) {
-        System.out.println("function9(List<T> var)");
+        System.out.println("genericsType2Arg");
     }
 
     public void function10(List<Integer> var) {
-        System.out.println("function10(List<Integer> var)");
+        System.out.println("genericsIntegerArg");
     }
 }
