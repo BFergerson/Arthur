6fe75d75ce9e97ec5b53699b8d98a25d2ea7399e
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-06-11 23:24:45 -0600

    revert

--- src/main/groovy/com/codebrig/arthur/observe/structure/filter/NameFilter.groovy
+++ src/main/groovy/com/codebrig/arthur/observe/structure/filter/NameFilter.groovy
@@ -10,7 +10,6 @@ import com.codebrig.arthur.observe.structure.StructureFilter
  * @version 0.4
  * @since 0.2
  * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
- * @author <a href="mailto:valpecaoco@gmail.com">Val Pecaoco</a>
  */
 class NameFilter extends StructureFilter<NameFilter, String> {
 
@@ -25,15 +24,9 @@ class NameFilter extends StructureFilter<NameFilter, String> {
                 return evaluateProperty(node.token)
             }
 
-            def childNameFilter = null
+            def childNameFilter
             if (node.language == SourceLanguage.Javascript) {
-                switch (Objects.requireNonNull(node).internalType) {
-                    case "FunctionDeclaration":
-                        childNameFilter = new InternalRoleFilter("id").getFilteredNodes(node.children)
-                        break
-                    default:
-                        break
-                }
+                childNameFilter = new InternalRoleFilter("id").getFilteredNodes(node.children)
             } else if (node.language == SourceLanguage.Java) {
                 if (node.internalType == "VariableDeclarationStatement") {
                     MultiFilter.matchAll(
