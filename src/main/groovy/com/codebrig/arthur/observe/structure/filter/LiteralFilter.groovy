8d1488e5fb2d740fc4126ee9e4e06f9e650c0c67
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-06-20 19:41:15 -0600

    credit for idea

--- src/main/groovy/com/codebrig/arthur/observe/structure/filter/LiteralFilter.groovy
+++ src/main/groovy/com/codebrig/arthur/observe/structure/filter/LiteralFilter.groovy
@@ -8,7 +8,7 @@ import com.codebrig.arthur.observe.structure.StructureFilter
  *
  * @version 0.4
  * @since 0.4
- * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
+ * @author <a href="mailto:valpecaoco@gmail.com">Val Pecaoco</a>
  */
 class LiteralFilter extends StructureFilter<LiteralFilter, Void> {
 
