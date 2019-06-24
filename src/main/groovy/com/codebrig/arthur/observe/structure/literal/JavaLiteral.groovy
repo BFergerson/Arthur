64af34900137882fe5a1fd68a7096011e3ebb663
chess-equality <chessequality@gmail.com>
2019-06-24 16:45:56 +0800

    - [#32] Added string case

--- src/main/groovy/com/codebrig/arthur/observe/structure/literal/JavaLiteral.groovy
+++ src/main/groovy/com/codebrig/arthur/observe/structure/literal/JavaLiteral.groovy
@@ -36,6 +36,8 @@ class JavaLiteral extends StructureLiteral {
         switch (Objects.requireNonNull(node).internalType) {
             case "NumberLiteral":
                 return [numberValueLiteral(), doubleValueLiteral()]
+            case "StringLiteral":
+                return [stringValueLiteral()]
             default:
                 return Collections.emptyList()
         }
