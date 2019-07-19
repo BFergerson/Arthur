3722a97628a4148f2acc0d4202f0a74ec6dc3bcf
chess-equality <chessequality@gmail.com>
2019-07-19 17:59:40 +0800

    - [#32] Added initial implementation for PHP

--- src/main/groovy/com/codebrig/arthur/observe/structure/literal/PhpLiteral.groovy
+++ src/main/groovy/com/codebrig/arthur/observe/structure/literal/PhpLiteral.groovy
@@ -9,21 +9,48 @@ import com.codebrig.arthur.observe.structure.StructureLiteral
  * @version 0.4
  * @since 0.2
  * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
+ * @author <a href="mailto:valpecaoco@gmail.com">Val Pecaoco</a>
  */
 class PhpLiteral extends StructureLiteral {
 
     @Override
     String getNodeLiteralAttribute(SourceNode node) {
-        return null
+        switch (Objects.requireNonNull(node).internalType) {
+            case "Scalar_LNumber":
+                return numberValueLiteral()
+            case "Scalar_DNumber":
+                return doubleValueLiteral()
+            case "Scalar_String":
+                return stringValueLiteral()
+            default:
+                return null
+        }
     }
 
     @Override
     List<String> getPossibleNodeLiteralAttributes(SourceNode node) {
-        def literalAttribute = getNodeLiteralAttribute(node)
-        if (literalAttribute == null) {
-            return Collections.emptyList()
-        } else {
-            return Collections.singletonList(literalAttribute)
+        switch (Objects.requireNonNull(node).internalType) {
+            case "Scalar_LNumber":
+                return [numberValueLiteral()]
+            case "Scalar_DNumber":
+                return [doubleValueLiteral()]
+            case "StringLiteral":
+                return [stringValueLiteral()]
+            default:
+                return Collections.emptyList()
+        }
+    }
+
+    @Override
+    Object getNodeLiteralValue(SourceNode node) {
+        boolean isNegative = node.parentSourceNode.children.any { it.roles.any { it.negative } }
+        switch (node.getLiteralAttribute()) {
+            case numberValueLiteral():
+                return toLong(((isNegative) ? "-" : "") + node.token)
+            case doubleValueLiteral():
+                return toDouble(((isNegative) ? "-" : "") + node.token)
+            default:
+                return super.getNodeLiteralValue(node)
         }
     }
 }
---
+++ src/test/resources/same/literals/Literals.php
@@ -0,0 +1,9 @@
+<?php
+
+$param1 = 1;
+$param2 = "stringParam2";
+$param3 = 0b01111111100000000000000000000000;
+$param4 = 0777;
+$param5 = 0xFFFFFFFFFFFFFFFF;
+
+?>
