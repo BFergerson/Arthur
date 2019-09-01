2c90f227e6ab783db28f2cedf97d7667b06dcea6
chess-equality <chessequality@gmail.com>
2019-09-01 19:45:08 +0800

    - [#38] Added the rest of literals

--- src/main/groovy/com/codebrig/arthur/observe/structure/literal/BashLiteral.groovy
+++ src/main/groovy/com/codebrig/arthur/observe/structure/literal/BashLiteral.groovy
@@ -16,16 +16,9 @@ class BashLiteral extends StructureLiteral {
     String getNodeLiteralAttribute(SourceNode node) {
         switch (Objects.requireNonNull(node).internalType) {
             case "int_literal":
-                if (node.token.contains(".") ||
-                        (node.token.isDouble() &&
-                                (node.token.toUpperCase().contains("P")
-                                 || node.token.toUpperCase().contains("E")
-                                 || node.token.toUpperCase().endsWith("D")
-                                 || node.token.toUpperCase().endsWith("F"))
-                        )) {
-                    return doubleValueLiteral()
-                }
                 return numberValueLiteral()
+            case "word":
+                return getWordLiteralType(node)
             case "string_content":
                 return stringValueLiteral()
             default:
@@ -37,11 +30,62 @@ class BashLiteral extends StructureLiteral {
     List<String> getPossibleNodeLiteralAttributes(SourceNode node) {
         switch (Objects.requireNonNull(node).internalType) {
             case "int_literal":
-                return [numberValueLiteral(), doubleValueLiteral()]
+                return [numberValueLiteral()]
+            case "word":
+                return [getWordLiteralType(node)]
             case "string_content":
                 return [stringValueLiteral()]
             default:
                 return Collections.emptyList()
         }
+    }
+
+    @Override
+    Object getNodeLiteralValue(SourceNode node) {
+        def elements = getWordElements(node)
+        if (elements) {
+            def radix = elements[0]
+            def value = elements[1]
+            return new BigInteger(value, Integer.parseInt(radix)).longValue()
+        }
+        return node.token
+    }
+
+    static String getWordLiteralType(SourceNode node) {
+        String[] elements = getWordElements(node)
+        if (elements) {
+            return numberValueLiteral()
+        }
+        return stringValueLiteral()
+    }
+
+    static String[] getWordElements(SourceNode node) {
+        if (node.internalType == "word") {
+            def token = node.token.toUpperCase()
+            return parseLiteralToken(token)
+        }
+        return null
+    }
+
+    static String[] parseLiteralToken(String token) {
+        if ((token.startsWith("0") || token.startsWith("-0")) &&
+                !(token.startsWith("0X") || token.startsWith("-0X"))) {
+            def sign = token.startsWith("-0") ? "-" : ""
+            return ["8", sign + token.substring(1 + sign.length())]
+        } else if (token.startsWith("0X") || token.startsWith("-0X")) {
+            def n = token.startsWith("-0X") ? "-" : ""
+            return ["16", n + token.substring(2 + n.length())]
+        } else if (token.contains("#")) {
+            def array = token.split("#")
+            if (array.size() == 2) {
+                def isNegative = array[0].startsWith("-")
+                def value = isNegative ? "-"+array[1] : array[1]
+                def radix = isNegative ? array[0].substring(1) : array[0]
+                array[0] = radix
+                array[1] = value
+                return array
+            }
+        }
+        return null
     }
 }
--- src/test/groovy/com/codebrig/arthur/observe/structure/literal/BashLiteralTest.groovy
+++ src/test/groovy/com/codebrig/arthur/observe/structure/literal/BashLiteralTest.groovy
@@ -21,6 +21,36 @@ class BashLiteralTest extends ArthurTest {
     @Test
     void stringLiteralTest() {
         assertBashLiteralPresent("param2", "stringValue", "stringParam2")
+        assertBashLiteralPresent("param3", "stringValue", "9.234")
+    }
+
+    @Test
+    void binaryLiteralTest() {
+        assertBashLiteralPresent("param4", "numberValue", 0b1010000101000101101000010100010110100001010001011010000101000101L)
+        assertBashLiteralPresent("param5", "numberValue", 0b00101010)
+        assertBashLiteralPresent("param6", "numberValue", -0b00101010)
+    }
+
+    @Test
+    void octalLiteralTest() {
+        assertBashLiteralPresent("param7", "numberValue", 0123546263753256452432L)
+        assertBashLiteralPresent("param8", "numberValue", -0123546263753256452432L)
+    }
+
+    @Test
+    void hexadecimalLiteralTest() {
+        assertBashLiteralPresent("param9", "numberValue", 0xFFFFFFFFFFFF)
+        assertBashLiteralPresent("param10", "numberValue", 0xff1a618b7f65ea12L)
+        assertBashLiteralPresent("param11", "numberValue", -0x1000000)
+        assertBashLiteralPresent("param12", "numberValue", -0xc4ceb9fe1a85ec53L)
+    }
+
+    @Test
+    void otherBaseLiteralTest() {
+        assertBashLiteralPresent("param13", "numberValue", "4#123123")
+        assertBashLiteralPresent("param14", "numberValue", "-4#123123")
+        assertBashLiteralPresent("param15", "numberValue", "5#12341234")
+        assertBashLiteralPresent("param16", "numberValue", "-5#12341234")
     }
 
     private static void assertBashLiteralPresent(String literalName, String literalType, Object literalValue) {
@@ -38,7 +68,13 @@ class BashLiteralTest extends ArthurTest {
             if (literalNode.getLiteralAttribute() == BashLiteral.stringValueLiteral()) {
                 assertEquals(literalValue, literalNode.getLiteralValue())
             } else {
-                assertEquals(literalValue as double, literalNode.getLiteralValue() as double, 0.0)
+                String[] array = BashLiteral.parseLiteralToken(literalValue as String)
+                if (array) {
+                    def value = new BigInteger(array[1], Integer.parseInt(array[0])).longValue()
+                    assertEquals(value as double, literalNode.getLiteralValue() as double, 0.0)
+                } else {
+                    assertEquals(literalValue as double, literalNode.getLiteralValue() as double, 0.0)
+                }
             }
             foundLiteral = true
         }
